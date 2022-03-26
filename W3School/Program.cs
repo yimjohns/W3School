55230e6068c03415ae408be31e8355ccd8060b65
yimjohns <yimjohns.dev@gmail.com>
2022-03-26 19:30:41 +0100

    Common varialbe declarations

--- W3School/Program.cs
+++ W3School/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int myNum = 5; // For declaring an integer value - Integers have 4 bytes of memory allocation
+
+double myDoubleNum = 5.99D; // The D here can be ignored.
+
+char myLetter = 'D'; // Used for a single character
+
+bool myBool = true; // Has two values - true or false;
+
+string myText = "Hello";
+
+// To display all the declared variables, we can use Console.WriteLine
+
+Console.WriteLine("Hello " + myText + ", the first letter in Daniel is " + myLetter);
+Console.WriteLine(myNum); // Prints the value of myNum
+
+Console.WriteLine(myDoubleNum); // Observe it doesn't print out the D
+
+Console.WriteLine(myBool); // Prints True
+
+
+
+
+
+
