6dcc28e17b94346b81cbb6f08ef37b964779bd72
yimjohns <yimjohns.dev@gmail.com>
2022-03-26 19:19:49 +0100

    Different ways to comment line(s) of codes

--- W3School/Program.cs
+++ W3School/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+// This is one way to write comments in C#
+
+/* This is yet another way to write it
+ * However, this one allows you to do a multiline comment.
+ * In which case we can write more than one line of comments */
